cde48d503f5284a2110e650fe427ba600421bddb
Yigit Agca <yigitagcame@gmail.com>
2022-01-12 20:11:18 +0200

    Node logging module Morgan added

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const route = require('./routes');
 
 const Server = express();
 
+// Logging
+if (process.env.NODE_ENV !== 'production') {
+  const morgan = require('morgan');
+  Server.use(morgan('dev'));
+};
+
 // Routing
 Server.use('/', route);
 
