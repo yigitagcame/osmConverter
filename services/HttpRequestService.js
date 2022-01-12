293a52472ca8bc8a67c1e3b1a8cbf00850477d06
Yigit Agca <yigitagcame@gmail.com>
2022-01-12 18:24:22 +0200

    Http Request Service get method

--- services/HttpRequestService.js
+++ services/HttpRequestService.js
@@ -1,8 +1,16 @@
 const HttpRequestService = {};
+const axios = require('axios');
 
 HttpRequestService.get = async function(url, params = null) {
-  return true;
+  return await axios.get(url, {params: params}).then(function(res) {
+    return [res.status, res.data];
+  }).catch(function(error) {
+    if (error.response) {
+      return [error.response.status, error.response.data];
+    } else {
+      return [503, 'Service Unavailable!'];
+    }
+  });
 };
-
 
 module.exports = HttpRequestService;
--- tests/unit/HttpRequestService.test.js
+++ tests/unit/HttpRequestService.test.js
@@ -1,7 +1,22 @@
 const HttpRequestService = require('../../services/HttpRequestService');
 
 test('a successful http get request', async function() {
-  const getRequest = await HttpRequestService.get('https://google.com');
+  const [responseStatus, responseBody] = await HttpRequestService.get('https://google.com');
 
-  expect(getRequest).toBeTruthy();
+  expect(responseStatus).toBe(200);
+  expect(responseBody).toBeDefined();
+});
+
+test('a successful http get request to non exist page', async function() {
+  const [responseStatus, responseBody] = await HttpRequestService.get('https://www.google.com/brokenpath');
+
+  expect(responseStatus).toBe(404);
+  expect(responseBody).toBeDefined();
+});
+
+test('an unsuccessful http get request handled', async function() {
+  const [responseStatus, responseBody] = await HttpRequestService.get('https://www.');
+
+  expect(responseStatus).toBe(503);
+  expect(responseBody).toEqual('Service Unavailable!');
 });
